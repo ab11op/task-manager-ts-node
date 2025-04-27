3c4a5f579c68dc33017e01d31f1155919efebff0
Akash <akash@Akashs-MacBook-Air.local>
2025-04-27 15:57:28 +0530

    fix: mongo url

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 
-export const connectToDatabase = async() => {
 
-        await mongoose.connect(process.env.MONGO_URI as string).then(() => {
+export const connectToDatabase = async() => {
+        const MONGO_URI=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lvd25i9.mongodb.net/${process.env.DB_NAME}`
+        await mongoose.connect(MONGO_URI).then(() => {
             console.log('connected to the database')
         }).catch(err => {
             console.log('error in connecting to the database',err)
